6b4b2adde4d688f0d6d8272d0a81b72196e6a9bd
Haruki <nazawaharuki@gmail.com>
2023-03-03 07:56:41 +0900

    fix shift bug

--- src/utils/tagsParser.ts
+++ src/utils/tagsParser.ts
@@ -1,10 +1,10 @@
 /**
  * Cut out the tag types ("p", "e", etc...) from Nostr's tags multiple array and create an associative array with those as keys.
-*/
+ */
 export const parseTags = (tags: string[][]) => {
   return tags.reduce<{ [tagType: string]: string[] }>((prev, tagList) => {
-    tagList.shift();
     prev[tagList[0]] = tagList;
+    tagList.shift();
     return prev;
   }, {});
 };
