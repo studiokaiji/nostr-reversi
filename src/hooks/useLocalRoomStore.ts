aa614a776f3e8321bac6616ffb963d75cc127b84
Haruki <nazawaharuki@gmail.com>
2023-03-03 07:57:40 +0900

    add useState cache

--- src/hooks/useLocalRoomStore.ts
+++ src/hooks/useLocalRoomStore.ts
@@ -1,15 +1,19 @@
 import { useLiveQuery } from "dexie-react-hooks";
 import { db } from "@/db";
+import { useState } from "react";
 
 export const useLocalRoomStore = (roomId: string) => {
   const data = useLiveQuery(async () => {
     return await db.rooms.get(roomId);
   });
 
+  const [room, setLocalRoomState] = useState<Room | undefined>(data);
+
   const setRoom = async (room: Room) => {
-    await db.rooms.add(room, room.id);
+    await db.rooms.put(room, room.id);
+    setLocalRoomState(room);
     return room;
   };
 
-  return { data, setRoom };
+  return { room, setRoom };
 };
