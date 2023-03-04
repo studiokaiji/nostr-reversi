2d05986195c57b90ca68cd73e512619a38b08c0e
Haruki <nazawaharuki@gmail.com>
2023-03-04 21:28:19 +0900

    add relays

--- src/constants/nostr.ts
+++ src/constants/nostr.ts
@@ -3,8 +3,12 @@ export const CONTENT_BODY_VERSION = "0";
 export const CREATE_ROOM_E_TAG =
   "4669c3a8f4de837bd899f7069591978fa7c5d96caa7c7c1507a6678332543cc2";
 export const RELAYS_FOR_PROFILE = [
+  "wss://nostr-pub.wellorder.net",
   "wss://nos.lol",
+  "wss://relay.current.fyi",
   "wss://nostr.wine",
   "wss://relay.nostr.wirednet.jp",
   "wss://nostr.mom",
+  "wss://nostr.sandwich.farm",
+  "wss://relay.nostr.bg",
 ];
