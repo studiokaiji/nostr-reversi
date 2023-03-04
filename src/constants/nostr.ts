3cc42202f8189ec9d33e7d720f46d8e7cfbdf392
Haruki <nazawaharuki@gmail.com>
2023-03-04 17:01:31 +0900

    add RELAYS_FOR_PROFILE

--- src/constants/nostr.ts
+++ src/constants/nostr.ts
@@ -1,2 +1,10 @@
 export const KIND = 64;
 export const CONTENT_BODY_VERSION = "0";
+export const CREATE_ROOM_E_TAG =
+  "4669c3a8f4de837bd899f7069591978fa7c5d96caa7c7c1507a6678332543cc2";
+export const RELAYS_FOR_PROFILE = [
+  "wss://nos.lol",
+  "wss://nostr.wine",
+  "wss://relay.nostr.wirednet.jp",
+  "wss://nostr.mom",
+];
