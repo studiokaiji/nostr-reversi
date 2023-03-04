d1029343354b12d4ca41bcc636e391e7bdb8df4f
Haruki <nazawaharuki@gmail.com>
2023-03-04 21:28:12 +0900

    add ProfileCard

---
+++ src/components/helpers/ProfileCard.tsx
@@ -0,0 +1,52 @@
+import { Disc } from "./Disc";
+
+type ProfileCardProps = {
+  disc?: Disc;
+  profile: Profile;
+  npub: string;
+  numberOfDisc: number;
+};
+
+export const ProfileCard = ({
+  disc,
+  profile,
+  npub,
+  numberOfDisc,
+}: ProfileCardProps) => (
+  <a
+    css={{
+      boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+      padding: "12px",
+      display: "flex",
+      gap: "7px",
+      alignItems: "center",
+      ":hover": {
+        opacity: 1,
+      },
+      borderRadius: "3px",
+    }}
+    href={`https://iris.to/${npub}`}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {disc && <Disc color={disc} isDisplayBorder />}
+    {profile?.picture ? (
+      <img
+        src={profile?.picture}
+        width={42}
+        height={42}
+        css={{ borderRadius: "2px" }}
+      />
+    ) : (
+      <div
+        css={{
+          backgroundColor: "lightGray",
+          width: 42,
+          height: 42,
+          borderRadius: "2px",
+        }}
+      />
+    )}
+    <div css={{ color: "black" }}>{numberOfDisc}</div>
+  </a>
+);
