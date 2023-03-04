2baf7cc3554e55155a445672a90000b461414167
Haruki <nazawaharuki@gmail.com>
2023-03-04 17:38:32 +0900

    change profile type

--- @types/Profile.d.ts
+++ @types/Profile.d.ts
@@ -2,6 +2,14 @@ type Profile = {
   name: string;
   username: string;
   display_name: string;
+  displayName: string;
+  picture: string;
+  banner: string;
+  website: string;
+  about: string;
+  nip05: string;
+  lud16: string;
+  lud06: string;
   created_at: number;
   nip05valid: boolean;
 };
