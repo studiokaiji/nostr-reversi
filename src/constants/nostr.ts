2a4ba2e28020fcfb2ae184e0f279d75cee24c87b
Haruki <nazawaharuki@gmail.com>
2023-03-11 14:51:29 +0900

    change kind

--- src/constants/nostr.ts
+++ src/constants/nostr.ts
@@ -1,4 +1,4 @@
-export const KIND = 64;
+export const KIND = 6464;
 export const CONTENT_BODY_VERSION = "0";
 export const CREATE_ROOM_E_TAG =
   "4669c3a8f4de837bd899f7069591978fa7c5d96caa7c7c1507a6678332543cc2";
