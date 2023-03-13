dea991755b31ef5b55e474e7c879526668bae94e
Haruki <nazawaharuki@gmail.com>
2023-03-14 02:24:23 +0900

    add Outlet

--- src/components/helpers/NothelloLogo.tsx
+++ src/components/helpers/NothelloLogo.tsx
@@ -1,26 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export const NothelloLogo = () => (
-  <Link css={{ userSelect: "none", color: "black" }} to="/">
-    <div css={{ display: "inline-flex", flexDirection: "column" }}>
-      <h1 css={{ wordWrap: "break-word" }}>
-        <div
+  <>
+    <Link css={{ userSelect: "none", color: "black" }} to="/">
+      <div css={{ display: "inline-flex", flexDirection: "column" }}>
+        <h1 css={{ wordWrap: "break-word" }}>
+          <div
+            css={{
+              letterSpacing: "-0.25em",
+              marginRight: 20,
+            }}
+          >
+            ⚪️⚫️
+          </div>
+          nothello
+        </h1>
+        <p
           css={{
-            letterSpacing: "-0.25em",
-            marginRight: 20,
+            display: "inline",
           }}
         >
-          ⚪️⚫️
-        </div>
-        nothello
-      </h1>
-      <p
-        css={{
-          display: "inline",
-        }}
-      >
-        Reversi working on Nostr.
-      </p>
-    </div>
-  </Link>
+          Reversi working on Nostr.
+        </p>
+      </div>
+    </Link>
+    <Outlet />
+  </>
 );
