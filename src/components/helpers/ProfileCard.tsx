62e119f04c2d8fa4267c77eb6b71dfe3eb0d8f16
Haruki <nazawaharuki@gmail.com>
2023-03-14 02:49:18 +0900

    add Card

--- src/components/helpers/ProfileCard.tsx
+++ src/components/helpers/ProfileCard.tsx
@@ -1,5 +1,6 @@
 import { Disc } from "./Disc";
 import Skeleton from "react-loading-skeleton";
+import { Card } from "./Card";
 
 type ProfileCardProps = {
   disc?: Disc;
@@ -16,31 +17,27 @@ export const ProfileCard = ({
 }: ProfileCardProps) => (
   <a
     css={{
-      boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
-      padding: "12px",
-      display: "flex",
-      gap: "7px",
-      alignItems: "center",
       ":hover": {
         opacity: 1,
       },
-      borderRadius: "3px",
     }}
     href={`https://iris.to/${npub}`}
     target="_blank"
     rel="noopener noreferrer"
   >
-    {disc && <Disc color={disc} isDisplayBorder />}
-    {profile ? (
-      <img
-        src={profile?.picture || ""}
-        width={42}
-        height={42}
-        css={{ borderRadius: "2px" }}
-      />
-    ) : (
-      <Skeleton width={42} height={42} borderRadius="2px" />
-    )}
-    {numberOfDisc && <div css={{ color: "black" }}>{numberOfDisc}</div>}
+    <Card>
+      {disc && <Disc color={disc} isDisplayBorder />}
+      {profile ? (
+        <img
+          src={profile?.picture || ""}
+          width={42}
+          height={42}
+          css={{ borderRadius: "2px" }}
+        />
+      ) : (
+        <Skeleton width={42} height={42} borderRadius="2px" />
+      )}
+      {numberOfDisc && <div css={{ color: "black" }}>{numberOfDisc}</div>}
+    </Card>
   </a>
 );
