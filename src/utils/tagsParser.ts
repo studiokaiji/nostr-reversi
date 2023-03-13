9ec06b44a0c4c6e61b53f91a2e7dccbffac86418
Haruki <nazawaharuki@gmail.com>
2023-03-14 01:38:49 +0900

    copieable to tags array

--- src/utils/tagsParser.ts
+++ src/utils/tagsParser.ts
@@ -2,9 +2,11 @@
  * Cut out the tag types ("p", "e", etc...) from Nostr's tags multiple array and create an associative array with those as keys.
  */
 export const parseTags = (tags: string[][]) => {
-  return tags.reduce<{ [tagType: string]: string[] }>((prev, tagList) => {
-    prev[tagList[0]] = tagList;
-    tagList.shift();
-    return prev;
-  }, {});
+  return tags
+    .map((t) => [...t])
+    .reduce<{ [tagType: string]: string[] }>((prev, tagList) => {
+      prev[tagList[0]] = tagList;
+      tagList.shift();
+      return prev;
+    }, {});
 };
