47631dfd7dcf878c9eee64c68975303810e8d738
Haruki <nazawaharuki@gmail.com>
2023-03-13 23:30:03 +0900

    add skelton

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "react-loading-skeleton/dist/skeleton.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { IndexPage } from "./pages";
 import { RoomPage } from "./pages/rooms/room";
